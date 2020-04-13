77dbeaab0d80622091f6f3505815a56b393c8ca3
clydefisher <61527357+clydefisher@users.noreply.github.com>
2020-04-13 19:35:51 +1000

    sad

---
+++ Arrays and Lists.cs
@@ -0,0 +1,27 @@
+using System; 
+
+
+    namespace Arrays
+    {
+
+        class Program 
+        {
+
+            static void Main(strin[]args)
+            {
+                // declaring array
+                int[] numbersa = new int[5];
+
+                nameof[2] = John;
+                nameof[0] = Jacob;
+                nameof[1] = Kira;
+                nameof[3] = Sally;
+                nameof[4] = Shay;
+
+                int index = 0; 
+                while (index < numbersa,length)
+                    System.Console.WriteLine("Hello" nameof[<5]);  
+            }
+        }
+    
+    }
